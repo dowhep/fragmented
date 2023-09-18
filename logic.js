bd9c2bdc65829a1d0cbbcecef316837868aefdc3
Marcelo Shen <caogangshen@gmail.com>
2023-09-17 23:03:20 -0700

    following webgl tutorial

---
+++ logic.js
@@ -0,0 +1,23 @@
+
+export function generatePuzzle() {
+  
+}
+
+/**
+ * Update item attached to mouse when mouse clicked
+ * @param {*} mousepos mouse position
+ */
+export function onClicked(mousepos) {
+  // if already attacked
+  // check if placeable, yes => place, no => return previous position
+  // else 
+  // check if on shape, yes => attach shape, no => do nothing
+}
+
+/**
+ * Update current shape position based on mouse position
+ * @param {*} mousepos mouse position
+ */
+export function snapShape(mousepos) {
+  // if shape snapped, update shape to mouse pos
+}
